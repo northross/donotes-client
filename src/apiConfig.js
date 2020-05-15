c6209cb29d2dc76f1f4c9313694d05ba7059f46f
northross <rossnorthrup@yahoo.com>
2020-05-15 12:20:01 -0400

    Added backend url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://evening-bayou-19427.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
